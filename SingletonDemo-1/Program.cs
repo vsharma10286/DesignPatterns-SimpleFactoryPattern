9ca2c4166d5bca14dcd30b2db9438ef314969cf8
Varun <vsharma10286@gmail.com>
2019-12-28 08:18:22 +0530

    Singleton Pattern with Lazy Loading Implemented

---
+++ SingletonDemo-1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+                () => FromS1(),
+                () => FromS2()
+                );
+
+            Console.ReadLine();
+
+        }
+
+        private static void FromS2()
+        {
+            Singleton s2 = Singleton.getInstance;
+
+            s2.PrintMessage("Second Message");
+        }
+
+        private static void FromS1()
+        {
+            Singleton s1 = Singleton.getInstance;
+
+            s1.PrintMessage("First Message");
+        }
+    }
+}
---
+++ SingletonDemo-1/Singleton.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo_1
+{
+    //sealed restrict the inhertitance
+
+  public sealed  class Singleton
+    {
+        private  int counter;
+
+        // below is example of eager loading , when object is available before it is use //
+        //private static readonly Singleton instance = new Singleton();
+
+
+        private static readonly Lazy<Singleton> instance = new Lazy<Singleton>(()=> new Singleton());
+
+
+
+        public static Singleton getInstance
+        {
+            get
+            {              
+                return instance.Value;
+            }
+        }
+
+        /*  
+         *  Private COnstructor ensures that Object is not instantiated other than with in the class itself
+         *  */
+
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine(counter);
+        }
+
+        public void PrintMessage(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
